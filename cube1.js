d261099a3ea1dabd717940a6272bf572051a8a92
박정현 <>
2020-12-07 19:19:55 +0900

    no message

--- cube1.js
+++ cube1.js
@@ -1,17 +1,3 @@
-/*1단계: 단어 밀어내기 구현하기
-입력: 사용자로부터 단어 하나, 정수 숫자 하나( -100 <= N < 100) , L 또는 R을 입력받는다. L 또는 R은 대소문자 모두 입력 가능하다.
-출력 예시
-> apple 3 L 
-leapp
-
-> banana 6 R
-banana
-
-> carrot -1 r
-arrotc
-
-> cat -4 R
-atc*/
 function main() {
     const result = document.getElementById('result');
     const word = document.getElementById('word').value;
