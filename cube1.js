7b14e2b38a38d1cb829716e6d4f508d34f194b04
박정현 <>
2020-12-07 19:00:02 +0900

    1. 단어 밀어내기 구현

--- cube1.js
+++ cube1.js
@@ -1,5 +1,6 @@
 /*1단계: 단어 밀어내기 구현하기
 입력: 사용자로부터 단어 하나, 정수 숫자 하나( -100 <= N < 100) , L 또는 R을 입력받는다. L 또는 R은 대소문자 모두 입력 가능하다.
+출력 예시
 > apple 3 L 
 leapp
 
@@ -10,8 +11,7 @@ banana
 arrotc
 
 > cat -4 R
-atc
-*/
+atc*/
 function main() {
     const result = document.getElementById('result');
     const word = document.getElementById('word').value;
@@ -21,15 +21,18 @@ function main() {
     let wordArr = word.split('');
     let absNum = Math.abs(num);
 
-    if ((direction === 'L' && num === absNum) || (direction === 'R' && num !== absNum)) 
-    {
-        let leftSpc = wordArr.splice(absNum % wordArr.length); 
-        result.innerHTML = `${leftSpc.join('') + wordArr.join('')}`;
-    } 
-    else if ((direction === 'L' && num !== absNum) || (direction === 'R' && num === absNum)) 
-    {
-        let rightSpc = wordArr.splice(wordArr.length - (absNum % wordArr.length));
-        result.innerHTML = `${rightSpc.join('') + wordArr.join('')}`;
+    if (num < -100 || num >= 100) {
+        alert('정수값을 -100이상 100미만으로 입력해주세요.');
+    }
+
+    if ((direction === 'L' && num == absNum) || (direction === 'R' && num != absNum)) {
+        let left = wordArr.splice(absNum % wordArr.length);
+        result.innerHTML = `> ${word} ${num} ${direction}<br>
+            ${left.join('') + wordArr.join('')}`;
+    } else {
+        let right = wordArr.splice(wordArr.length - (absNum % wordArr.length));
+        result.innerHTML = `> ${word} ${num} ${direction}<br>
+            ${right.join('') + wordArr.join('')}`;
     }
 }
 main();
